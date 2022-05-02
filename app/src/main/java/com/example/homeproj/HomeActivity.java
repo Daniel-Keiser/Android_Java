7663308a0dca44ee502b61db1e91d9390643a471
Daniel Strey <daniel.ks100@gmail.com>
2022-05-02 19:48:04 -0300

    Aula 2

--- app/src/main/java/com/example/homeproj/HomeActivity.java
+++ app/src/main/java/com/example/homeproj/HomeActivity.java
@@ -3,12 +3,47 @@ package com.example.homeproj;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.ListView;
+import android.widget.SimpleAdapter;
+import android.widget.TextView;
 
 public class HomeActivity extends AppCompatActivity {
-
+    static String[] nomes = new String[] {"valmor", "antonio", "ricardo", "reginaldo"};
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        getSupportActionBar().hide();
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+        ListView lv = findViewById(R.id.listUser);
+        lv.setAdapter(new BaseAdapter() {
+                @Override
+                public int getCount() {
+                    return nomes.length;
+                }
+
+                @Override
+                public Object getItem(int i) {
+                    return nomes[i];
+                }
+
+                @Override
+                public long getItemId(int i) {
+                    return i;
+                }
+
+                @Override
+                public View getView(int i, View view, ViewGroup viewGroup) {
+                    if(view == null) {
+                        view = getLayoutInflater().inflate(R.layout.layout_linha, viewGroup, false);
+                    }
+                    TextView temp = view.findViewById(R.id.txtPrincipal);
+                    temp.setText(nomes[i]);
+
+                    return view;
+                }
+            });
+        }
     }
-}
